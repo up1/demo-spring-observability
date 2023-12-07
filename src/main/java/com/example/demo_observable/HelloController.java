4879981e666fcfbe63875edf9c6a561f2dab938a
up1 <somkiat.p@gmail.com>
2023-12-07 22:26:12 +0700

    Add logback

--- src/main/java/com/example/demo_observable/HelloController.java
+++ src/main/java/com/example/demo_observable/HelloController.java
@@ -10,10 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static net.logstash.logback.argument.StructuredArguments.entries;
+import static net.logstash.logback.argument.StructuredArguments.keyValue;
+import static net.logstash.logback.marker.Markers.append;
+import static net.logstash.logback.marker.Markers.appendFields;
+
 @RestController
 public class HelloController {
 
     private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+//    private static final Logger log = LoggerFactory.getLogger("json-log");
 
     @Autowired
     private HelloService helloService;
@@ -22,9 +32,19 @@ public class HelloController {
     @ResponseStatus(HttpStatus.OK)
     @Observed(name = "hello-controller", contextualName = "say-hi")
     public String sayHi() {
+        // Simple log
         log.info("Called function sayHi");
+
+        // Structured log
+        log.info("Order saved {}", keyValue("orderId", "0001"));
+        MyData myData = new MyData("value1", "value2");
+        log.info("Order saved {}", keyValue("my_data", myData));
+        log.info("Order saved {}", keyValue("orderId", "0001"), keyValue("my_data", myData));
+
         helloService.doSth();
         return "Hello Spring Boot";
     }
 
 }
+
+
---
+++ src/main/java/com/example/demo_observable/MyData.java
@@ -0,0 +1,7 @@
+package com.example.demo_observable;
+
+public record MyData(
+	String data2,
+	String data1
+) {
+}
