af357ff3fd86e6325ea78ee6e5f080d8ba87a063
up1 <somkiat.p@gmail.com>
2023-12-07 08:46:21 +0700

    Custom status code

--- src/main/java/com/example/demo_observable/HelloController.java
+++ src/main/java/com/example/demo_observable/HelloController.java
@@ -5,7 +5,9 @@ import io.micrometer.observation.annotation.Observed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,7 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseStatus(HttpStatus.OK)
     @Observed(name = "hello-controller", contextualName = "say-hi")
     public String sayHi() {
         log.info("Called function sayHi");
